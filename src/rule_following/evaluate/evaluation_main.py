6ecbfbea843c4293260fdfa1c28076675b0297a2
Zhihan Zhang <zhangzhihan719@qq.com>
2024-09-22 19:25:42 +0000

    reformat the code with black

--- src/rule_following/evaluate/evaluation_main.py
+++ src/rule_following/evaluate/evaluation_main.py
@@ -1,4 +1,3 @@
-
 import collections
 import dataclasses
 import json
@@ -13,6 +12,7 @@ import instructions_registry
 
 import colorama
 from termcolor import colored
+
 colorama.init()
 
 
@@ -34,238 +34,237 @@ _OUTPUT_DIR = flags.DEFINE_string(
 
 @dataclasses.dataclass
 class InputExample:
-  key: int
-  instruction_id_list: list[str]
-  system: str
-  prompt: str
-  kwargs: list[Dict[str, Optional[Union[str, int]]]]
+    key: int
+    instruction_id_list: list[str]
+    system: str
+    prompt: str
+    kwargs: list[Dict[str, Optional[Union[str, int]]]]
 
 
 @dataclasses.dataclass
 class OutputExample:
-  key: int
-  instruction_id_list: list[str]
-  system: str
-  prompt: str
-  response: str
-  follow_all_instructions: bool
-  follow_instruction_list: list[bool]
+    key: int
+    instruction_id_list: list[str]
+    system: str
+    prompt: str
+    response: str
+    follow_all_instructions: bool
+    follow_instruction_list: list[bool]
 
 
 def read_prompt_list(input_json_filename):
-  """Read inputs from json."""
-  inputs = []
-  data = json.load(open(input_json_filename, "r", encoding="utf-8"))
+    """Read inputs from json."""
+    inputs = []
+    data = json.load(open(input_json_filename, "r", encoding="utf-8"))
 
-  for example in data:
-    inputs.append(InputExample(key=example["id"],
-                  instruction_id_list=example["answer"]["instruction_id_list"],
-                  system=example["system"] if "system" in example else None,
-                  prompt=example["instruction"],
-                  kwargs=example["answer"]["kwargs"]))
-  return inputs
+    for example in data:
+        inputs.append(
+            InputExample(
+                key=example["id"],
+                instruction_id_list=example["answer"]["instruction_id_list"],
+                system=example["system"] if "system" in example else None,
+                prompt=example["instruction"],
+                kwargs=example["answer"]["kwargs"],
+            )
+        )
+    return inputs
 
 
 def write_outputs(output_jsonl_filename, outputs):
-  """Writes outputs to jsonl."""
-  assert outputs
-  results = []
-  for o in outputs:
-      results.append(
-              {
-                  attr_name: o.__getattribute__(attr_name)
-                  for attr_name in [
-                      name for name in dir(o) if not name.startswith("_")
-                  ]
-              }
-          )
-  # sort results by key
-  results = sorted(results, key=lambda x: x["key"])
-  json.dump(results, open(output_jsonl_filename, "w"), indent=4, ensure_ascii=False)
+    """Writes outputs to jsonl."""
+    assert outputs
+    results = []
+    for o in outputs:
+        results.append(
+            {
+                attr_name: o.__getattribute__(attr_name)
+                for attr_name in [name for name in dir(o) if not name.startswith("_")]
+            }
+        )
+    # sort results by key
+    results = sorted(results, key=lambda x: x["key"])
+    json.dump(results, open(output_jsonl_filename, "w"), indent=4, ensure_ascii=False)
 
 
 def test_instruction_following_strict(
     inp,
     prompt_to_response,
 ):
-  """Tests response to see if instrutions are followed."""
-  response = prompt_to_response[inp.prompt]
-  instruction_list = inp.instruction_id_list
-  is_following_list = []
+    """Tests response to see if instrutions are followed."""
+    response = prompt_to_response[inp.prompt]
+    instruction_list = inp.instruction_id_list
+    is_following_list = []
 
-  for index, instruction_id in enumerate(instruction_list):
-    instruction_cls = instructions_registry.INSTRUCTION_DICT[instruction_id]
-    instruction = instruction_cls(instruction_id)
+    for index, instruction_id in enumerate(instruction_list):
+        instruction_cls = instructions_registry.INSTRUCTION_DICT[instruction_id]
+        instruction = instruction_cls(instruction_id)
 
-    instruction.build_description(**inp.kwargs[index])
-    args = instruction.get_instruction_args()
-    if args and "prompt" in args:
-      instruction.build_description(prompt=inp.prompt)
+        instruction.build_description(**inp.kwargs[index])
+        args = instruction.get_instruction_args()
+        if args and "prompt" in args:
+            instruction.build_description(prompt=inp.prompt)
 
-    if response.strip() and instruction.check_following(response):
-      is_following_list.append(True)
-    else:
-      is_following_list.append(False)
+        if response.strip() and instruction.check_following(response):
+            is_following_list.append(True)
+        else:
+            is_following_list.append(False)
 
-  return OutputExample(
-      key=inp.key,
-      instruction_id_list=inp.instruction_id_list,
-      system=inp.system,
-      prompt=inp.prompt,
-      response=response,
-      follow_all_instructions=all(is_following_list),
-      follow_instruction_list=is_following_list,
-  )
+    return OutputExample(
+        key=inp.key,
+        instruction_id_list=inp.instruction_id_list,
+        system=inp.system,
+        prompt=inp.prompt,
+        response=response,
+        follow_all_instructions=all(is_following_list),
+        follow_instruction_list=is_following_list,
+    )
 
 
 def test_instruction_following_loose(
     inp,
     prompt_to_response,
 ):
-  """Tests response for an upper bound for following instructions."""
-  response = prompt_to_response[inp.prompt]
-  r = response.split("\n")
-  response_remove_first = "\n".join(r[1:]).strip()
-  response_remove_last = "\n".join(r[:-1]).strip()
-  response_remove_both = "\n".join(r[1:-1]).strip()
-  revised_response = response.replace("*", "")
-  revised_response_remove_first = response_remove_first.replace("*", "")
-  revised_response_remove_last = response_remove_last.replace("*", "")
-  revised_response_remove_both = response_remove_both.replace("*", "")
-  all_responses = [
-      response,
-      revised_response,
-      response_remove_first,
-      response_remove_last,
-      response_remove_both,
-      revised_response_remove_first,
-      revised_response_remove_last,
-      revised_response_remove_both,
-  ]
-  instruction_list = inp.instruction_id_list
-  is_following_list = []
+    """Tests response for an upper bound for following instructions."""
+    response = prompt_to_response[inp.prompt]
+    r = response.split("\n")
+    response_remove_first = "\n".join(r[1:]).strip()
+    response_remove_last = "\n".join(r[:-1]).strip()
+    response_remove_both = "\n".join(r[1:-1]).strip()
+    revised_response = response.replace("*", "")
+    revised_response_remove_first = response_remove_first.replace("*", "")
+    revised_response_remove_last = response_remove_last.replace("*", "")
+    revised_response_remove_both = response_remove_both.replace("*", "")
+    all_responses = [
+        response,
+        revised_response,
+        response_remove_first,
+        response_remove_last,
+        response_remove_both,
+        revised_response_remove_first,
+        revised_response_remove_last,
+        revised_response_remove_both,
+    ]
+    instruction_list = inp.instruction_id_list
+    is_following_list = []
 
-  for index, instruction_id in enumerate(instruction_list):
-    instruction_cls = instructions_registry.INSTRUCTION_DICT[instruction_id]
-    instruction = instruction_cls(instruction_id)
+    for index, instruction_id in enumerate(instruction_list):
+        instruction_cls = instructions_registry.INSTRUCTION_DICT[instruction_id]
+        instruction = instruction_cls(instruction_id)
 
-    instruction.build_description(**inp.kwargs[index])
-    args = instruction.get_instruction_args()
-    if args and "prompt" in args:
-      instruction.build_description(prompt=inp.prompt)
+        instruction.build_description(**inp.kwargs[index])
+        args = instruction.get_instruction_args()
+        if args and "prompt" in args:
+            instruction.build_description(prompt=inp.prompt)
 
-    is_following = False
-    for r in all_responses:
-      if r.strip() and instruction.check_following(r):
-        is_following = True
-        break
+        is_following = False
+        for r in all_responses:
+            if r.strip() and instruction.check_following(r):
+                is_following = True
+                break
 
-    is_following_list.append(is_following)
+        is_following_list.append(is_following)
 
-  return OutputExample(
-      key=inp.key,
-      instruction_id_list=inp.instruction_id_list,
-      system=inp.system,
-      prompt=inp.prompt,
-      response=response,
-      follow_all_instructions=all(is_following_list),
-      follow_instruction_list=is_following_list,
-  )
+    return OutputExample(
+        key=inp.key,
+        instruction_id_list=inp.instruction_id_list,
+        system=inp.system,
+        prompt=inp.prompt,
+        response=response,
+        follow_all_instructions=all(is_following_list),
+        follow_instruction_list=is_following_list,
+    )
 
 
 def read_prompt_to_response_dict(input_jsonl_filename):
-  """Creates dictionary matching prompt and response."""
-  return_dict = {}
-  with open(input_jsonl_filename, "r") as f:
-    for l in f:
-      example = json.loads(l)
-      return_dict[example["prompt"]] = example["response"]
-  return return_dict
+    """Creates dictionary matching prompt and response."""
+    return_dict = {}
+    with open(input_jsonl_filename, "r") as f:
+        for l in f:
+            example = json.loads(l)
+            return_dict[example["prompt"]] = example["response"]
+    return return_dict
 
 
 def print_report(outputs):
-  """Prints a report on accuracy scores."""
+    """Prints a report on accuracy scores."""
 
-  prompt_total = 0
-  prompt_correct = 0
-  instruction_total = 0
-  instruction_correct = 0
+    prompt_total = 0
+    prompt_correct = 0
+    instruction_total = 0
+    instruction_correct = 0
 
-  tier0_total = collections.defaultdict(int)
-  tier0_correct = collections.defaultdict(int)
+    tier0_total = collections.defaultdict(int)
+    tier0_correct = collections.defaultdict(int)
 
-  tier1_total = collections.defaultdict(int)
-  tier1_correct = collections.defaultdict(int)
+    tier1_total = collections.defaultdict(int)
+    tier1_correct = collections.defaultdict(int)
 
-  for example in outputs:
-    follow_instruction_list = example.follow_instruction_list
-    instruction_id_list = example.instruction_id_list
+    for example in outputs:
+        follow_instruction_list = example.follow_instruction_list
+        instruction_id_list = example.instruction_id_list
 
-    prompt_total += 1
-    if all(follow_instruction_list):
-      prompt_correct += 1
+        prompt_total += 1
+        if all(follow_instruction_list):
+            prompt_correct += 1
 
-    instruction_total += len(instruction_id_list)
-    instruction_correct += sum(follow_instruction_list)
+        instruction_total += len(instruction_id_list)
+        instruction_correct += sum(follow_instruction_list)
 
-    for instruction_id, followed_or_not in zip(
-        instruction_id_list, follow_instruction_list
-    ):
-      instruction_id = instruction_id.split(":")[0]
-      tier0_total[instruction_id] += 1
-      if followed_or_not:
-        tier0_correct[instruction_id] += 1
+        for instruction_id, followed_or_not in zip(
+            instruction_id_list, follow_instruction_list
+        ):
+            instruction_id = instruction_id.split(":")[0]
+            tier0_total[instruction_id] += 1
+            if followed_or_not:
+                tier0_correct[instruction_id] += 1
 
-    for instruction_id, followed_or_not in zip(
-        instruction_id_list, follow_instruction_list
-    ):
-      tier1_total[instruction_id] += 1
-      if followed_or_not:
-        tier1_correct[instruction_id] += 1
+        for instruction_id, followed_or_not in zip(
+            instruction_id_list, follow_instruction_list
+        ):
+            tier1_total[instruction_id] += 1
+            if followed_or_not:
+                tier1_correct[instruction_id] += 1
 
-  print(f"prompt-level: {prompt_correct / prompt_total:.2%}")
-  print(f"instruction-level: {instruction_correct / instruction_total:.2%}")
-  # Uncomment to print more detailed scores
-  # print()
-  # for instruction_id in sorted(tier0_total.keys()):
-  #   accuracy = tier0_correct[instruction_id] / tier0_total[instruction_id]
-  #   print(f"{instruction_id} {accuracy:.2%}")
-  # print()
-  # for instruction_id in sorted(tier1_total.keys()):
-  #   accuracy = tier1_correct[instruction_id] / tier1_total[instruction_id]
-  #   print(f"{instruction_id} {accuracy:.2%}")
+    print(f"prompt-level: {prompt_correct / prompt_total:.2%}")
+    print(f"instruction-level: {instruction_correct / instruction_total:.2%}")
+    # Uncomment to print more detailed scores
+    # print()
+    # for instruction_id in sorted(tier0_total.keys()):
+    #   accuracy = tier0_correct[instruction_id] / tier0_total[instruction_id]
+    #   print(f"{instruction_id} {accuracy:.2%}")
+    # print()
+    # for instruction_id in sorted(tier1_total.keys()):
+    #   accuracy = tier1_correct[instruction_id] / tier1_total[instruction_id]
+    #   print(f"{instruction_id} {accuracy:.2%}")
 
 
 def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError("Too many command-line arguments.")
+    if len(argv) > 1:
+        raise app.UsageError("Too many command-line arguments.")
 
-  inputs = read_prompt_list(_INPUT_DATA.value)
-  prompt_to_response = read_prompt_to_response_dict(
-      _INPUT_RESPONSE_DATA.value)
+    inputs = read_prompt_list(_INPUT_DATA.value)
+    prompt_to_response = read_prompt_to_response_dict(_INPUT_RESPONSE_DATA.value)
 
-  # get instruction following results
-  for func, output_file_name in [
-      (test_instruction_following_strict, "eval_results_strict"),
-      (test_instruction_following_loose, "eval_results_loose"),
-  ]:
-    outputs = []
-    for inp in inputs:
-      outputs.append(func(inp, prompt_to_response))
-    follow_all_instructions = [o.follow_all_instructions for o in outputs]
-    accuracy = sum(follow_all_instructions) / len(outputs)
+    # get instruction following results
+    for func, output_file_name in [
+        (test_instruction_following_strict, "eval_results_strict"),
+        (test_instruction_following_loose, "eval_results_loose"),
+    ]:
+        outputs = []
+        for inp in inputs:
+            outputs.append(func(inp, prompt_to_response))
+        follow_all_instructions = [o.follow_all_instructions for o in outputs]
+        accuracy = sum(follow_all_instructions) / len(outputs)
 
-    # Prints instruction following accuracy report.
-    print(colored("=" * 30 + output_file_name + "=" * 30, "green"))
-    print(f"{output_file_name} Accuracy Scores:")
-    print_report(outputs)
+        # Prints instruction following accuracy report.
+        print(colored("=" * 30 + output_file_name + "=" * 30, "green"))
+        print(f"{output_file_name} Accuracy Scores:")
+        print_report(outputs)
 
-    # Save the scores to file
-    output_file_name = os.path.join(
-        _OUTPUT_DIR.value, output_file_name + ".json"
-    )
-    write_outputs(output_file_name, outputs)
+        # Save the scores to file
+        output_file_name = os.path.join(_OUTPUT_DIR.value, output_file_name + ".json")
+        write_outputs(output_file_name, outputs)
 
 
 if __name__ == "__main__":
-  app.run(main)
+    app.run(main)
