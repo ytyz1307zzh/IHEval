36cd99e2d9cf20bebc7311ac9b769544a7ca41e6
Zhihan Zhang <zhangzhihan719@qq.com>
2024-09-24 20:07:17 +0000

    adjust the output effect of the final scores

--- src/model/average_final_score.py
+++ src/model/average_final_score.py
@@ -2,7 +2,6 @@
 Aggregate scores from different tasks
 """
 
-import pdb
 import json
 import argparse
 from statistics import mean
@@ -31,32 +30,40 @@ print(f"Loaded {len(data)} examples from {args.record}")
 
 results = {}
 for domain in data.keys():
-    all_task_scores = {"reference": [], "aligned": [], "conflict": []}
+
     for task, task_score_dict in data[domain].items():
         task_ref_score = aggregate_task_score(task_score_dict['reference'])
         task_align_score = aggregate_task_score(task_score_dict['aligned'])
         task_conflict_score = aggregate_task_score(task_score_dict['conflict'])
 
-        all_task_scores['reference'].append(task_ref_score)
-        all_task_scores['aligned'].append(task_align_score)
-        all_task_scores['conflict'].append(task_conflict_score)
-
-    results[domain] = {
-        "reference": mean(all_task_scores['reference']),
-        "aligned": mean(all_task_scores['aligned']),
-        "conflict": mean(all_task_scores['conflict']),
-    }
+        results[f"{domain}_{task}"] = {
+            "reference": task_ref_score,
+            "aligned": task_align_score,
+            "conflict": task_conflict_score,
+        }
 
 results['overall'] = {
-    "reference": mean([results[domain]['reference'] for domain in data.keys()]),
-    "aligned": mean([results[domain]['aligned'] for domain in data.keys()]),
-    "conflict": mean([results[domain]['conflict'] for domain in data.keys()]),
+    "reference": mean([results[task]['reference'] for task in results.keys()]),
+    "aligned": mean([results[task]['aligned'] for task in results.keys()]),
+    "conflict": mean([results[task]['conflict'] for task in results.keys()]),
 }
 
 json.dump(results, open(args.output, 'w', encoding='utf8'), indent=4, ensure_ascii=False)
 print(f"Saved Overall Results to {args.output}")
 
 print()
-print(colored(f"Agg. Reference Score: {results['overall']['reference']}", "green"))
-print(colored(f"Agg. Aligned Score: {results['overall']['aligned']}", "green"))
-print(colored(f"Agg. Conflict Score: {results['overall']['conflict']}", "green"))
+for task, task_scores in results.items():
+    if task == 'overall':
+        continue
+    print(colored(f"Task: {task}", "red"))
+    print(colored(f"Reference: {task_scores['reference']:.1%}", "green"), end=", ")
+    print(colored(f"Aligned: {task_scores['aligned']:.1%}", "green"), end=", ")
+    print(colored(f"Conflict: {task_scores['conflict']:.1%}", "green"))
+    print()
+
+print(colored("Overall", "red"))
+print(colored(f"Agg. Reference: {results['overall']['reference']:.1%}", "green"), end=", ")
+print(colored(f"Agg. Aligned: {results['overall']['aligned']:.1%}", "green"), end=", ")
+print(colored(f"Agg. Conflict: {results['overall']['conflict']:.1%}", "green"))
+print(colored(f"Diff. Aligned: {results['overall']['aligned'] - results['overall']['reference']:.1%}", "yellow"), end=", ")
+print(colored(f"Diff. Conflict: {results['overall']['conflict'] - results['overall']['reference']:.1%}", "yellow"))
